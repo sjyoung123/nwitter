0d7cee0b0080b2dea92a23d30bcb874b8dd5be86
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-11-29 20:06:50 +0900

    Created account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,14 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "@firebase/auth";
+import { authService } from "fbase";
 import React, { useState } from "react";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
   const onChange = (event) => {
     const {
       target: { name, value },
@@ -13,8 +19,22 @@ const Auth = () => {
       setPassword(value);
     }
   };
-  const onSubmit = (event) => {
-    event.preventDefault();
+  const onSubmit = async (event) => {
+    let data;
+    try {
+      event.preventDefault();
+      if (newAccount) {
+        data = await createUserWithEmailAndPassword(
+          authService,
+          email,
+          password
+        );
+      } else {
+        data = await signInWithEmailAndPassword(authService, email, password);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -36,7 +56,7 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value="Log in" />
+        <input type="submit" value={newAccount ? "Create Account" : "Log in"} />
       </form>
       <div>
         <button>Continue with Google</button>
