9435a558e8d7ebd4eddfbfde9200b3d924936509
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-11-30 21:23:45 +0900

    Updated firestore DB.

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Profile from "routes/Profile";
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,39 @@
+import { addDoc, collection, serverTimestamp } from "@firebase/firestore";
+import { dbService } from "fbase";
 import React from "react";
+import { useState } from "react/cjs/react.development";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await addDoc(collection(dbService, "nweets"), {
+      nweet,
+      createAt: Date.now(),
+    });
+    setNweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={nweet}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
