c3e5ff3ae75ed94ffd62a3f3050121e9c94fea4b
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2021-11-30 21:59:41 +0900

    Updated get nweets in firestore

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,33 @@
-import { addDoc, collection, serverTimestamp } from "@firebase/firestore";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  serverTimestamp,
+} from "@firebase/firestore";
 import { dbService } from "fbase";
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react/cjs/react.development";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
+  useEffect(() => {
+    getNweets();
+  }, []);
+
+  const getNweets = async () => {
+    const q = query(collection(dbService, "nweets"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((document) => {
+      const nweetObj = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObj, ...prev]);
+    });
+  };
 
   const onSubmit = async (event) => {
     event.preventDefault();
@@ -33,6 +56,13 @@ const Home = () => {
         />
         <input type="submit" value="Nweet" />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
